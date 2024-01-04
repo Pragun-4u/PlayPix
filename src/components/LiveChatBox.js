1e3db4526a7c04d4e4392c5fbbbba7ead1f6e5c5
Pragun <chhayakarande18@gmail.com>
2024-01-04 19:14:28 +0530

    fix. API Key

--- src/components/LiveChatBox.js
+++ src/components/LiveChatBox.js
@@ -26,7 +26,7 @@ const LiveChatBox = () => {
     }, 2000);
 
     return () => clearInterval(i);
-  }, []);
+  });
 
   return (
     <>
@@ -41,12 +41,12 @@ const LiveChatBox = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 class="w-6 h-6"
               >
                 <path
-                  stroke-linecap="round"
+                  strokeLinecap="round"
                   stroke-linejoin="round"
                   d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
                 />
@@ -63,8 +63,8 @@ const LiveChatBox = () => {
                 class="w-6 h-6 "
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75"
                 />
               </svg>
